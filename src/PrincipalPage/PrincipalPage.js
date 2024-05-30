6ea7b3ce629dc2d1dacb1fbf2203391b8c66ca08
MrRixx69 <henrique.santarosa06@gmail.com>
2024-05-29 21:53:53 -0300

    FrontAdaption

--- src/PrincipalPage/PrincipalPage.js
+++ src/PrincipalPage/PrincipalPage.js
@@ -1,10 +1,13 @@
 import './PrincipalPage.css'
 
+
 import Inter from '../Imgs/Inter.jpeg';
 import Cooper from '../Imgs/Cooper.jpeg';
 import Brand from '../Imgs/Brand.jpeg';
 import Tom from '../Imgs/Tom.jpeg';
 import Murphy from '../Imgs/Murphy.jpeg';
+
+ 
 
 function PrincipalPage(){
 
@@ -13,53 +16,52 @@ function PrincipalPage(){
         <div className='bodyM'>
         
         <div class="container">
-        <header>
-            <h1>FilmFinder</h1>
+        <header className='hd'>
+            <h1 className='txc'>FilmFinder</h1>
             <nav>
-                <a href="#">Filmes</a>
+                <a href="#">Home</a>
                 <a href="#">Livros</a>
                 <a href="/register">Register</a>
             </nav>
         </header>
         <main>
             <section class="highlight">
-                <div class="movie-poster" ><img src={Inter} className='Inter'></img> </div>
+                <div class="movie-poster" > </div>
                 <div class="movie-info">
-                    <h2>Interestelar</h2>
-                    <div className='audience'><p><strong>Avaliação da audiência:</strong> 86%</p></div>
+                    <h2>Filmes API</h2>
+                    <div className='audience'><p><strong>Favoritos:</strong> -</p></div>
+                    <br />
+                    <div><p><strong>Categoria:</strong>-</p></div>
 
                     <div className='text-container'>
                         <p>
-                            As reservas naturais da Terra estão chegando ao fim e um grupo de astronautas recebe a missão
-                            de verificar possíveis planetas para receberem a população mundial,
-                            possibilitando a continuação da espécie.
+                            Sinopse 1
                         </p>
                         <p>
-                            Cooper é chamado para liderar o grupo e aceita a missão sabendo
-                             que pode nunca mais ver os filhos.
-                            Ao lado de Brand, Jenkins e Doyle, ele seguirá em busca de um novo lar.
+                            Sinopse 2
                      </p>
                     </div>
 
                 </div>
-                <div class="cast">
-                    <h3>Cast:</h3>
-                    <div class="actor" id="actor1"> <img src={Cooper} className='ac1'></img></div>
-                    <div class="actor" id="actor2"> <img src={Brand} className='ac2'></img></div>
-                    <div class="actor" id="actor3"> <img src={Tom} className='ac3'></img></div>
-                    <div class="actor" id="actor4"> <img src={Murphy} className='ac4'></img></div>
-                </div>
+               
             </section>
             <section class="recommendations">
-                <h3>Recomendações:</h3>
+                <h3 className='txc'>Recomendações:</h3>
+                <h3 className='txc'> Generos:</h3>
                 <div class="recommendations-container">
 
                     
-                        <div class="movie" id="movie1"></div>
-                        <div class="movie" id="movie2"></div>
-                        <div class="movie" id="movie3"></div>
-                        <div class="movie" id="movie4"></div>
-                        <div class="movie" id="movie5"></div>
+                        <div className="movie" id="movie1"></div>
+                        <div className="movie" id="movie2"></div>
+                        <div className="movie" id="movie3"></div>
+                        <div className="movie" id="movie4"></div>
+                        <div className="movie" id="movie5"></div>
+                        <div className="movie" id="movie6"></div>
+                        <div className="movie" id="movie7"></div>
+                        <div className="movie" id="movie8"></div>
+                        <div className="movie" id="movie9"></div>
+                        <div className="movie" id="movie10"></div>
+                        <div className="movie" id="movie11"></div>
                     
 
                 </div>
